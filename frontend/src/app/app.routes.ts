5cac70f06826d62bce003072ac8dd61b63e794ee
Alexander Belokon <alexander.belokon.mail@gmail.com>
2024-12-21 12:12:47 +0100

    Initial commit

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { TodoComponent } from './todo/todo.component';
+
+export const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'todo',
+    component: TodoComponent,
+  },
+];
