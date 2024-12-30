b42d188a98c9e403892aa531df1882f80d9268d5
zjouahri <49081437+ziko199@users.noreply.github.com>
2024-12-30 14:31:09 +0100

    gradle zu maven umstellen

--- backend/src/test/java/de/frankfurtuas/cybersec/BackendApplicationTests.java
+++ backend/src/test/java/de/frankfurtuas/cybersec/BackendApplicationTests.java
@@ -1,13 +1,13 @@
-package de.frankfurtuas.cybersec.passkeys;
+package de.frankfurtuas.cybersec;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PasskeysApplicationTests {
+class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
