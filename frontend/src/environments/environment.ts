ad5e7c8497886c4bed54a1457ae4d478ca6a9abb
Alexander Belokon <alexander.belokon.mail@gmail.com>
2025-01-19 17:33:57 +0100

    Successfully test passkey register and login

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
   businessServiceUrl: 'http://localhost:8081',
   keycloak: {
-    realm: 'test',
-    clientId: 'master',
+    realm: 'master',
+    clientId: 'test',
     url: 'http://localhost:8080',
   },
 };
